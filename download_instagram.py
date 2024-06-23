0214661fadbf6140124c0209866276f3c6c2765a
dalatilacerda <93090235+dalatilacerda@users.noreply.github.com>
2024-06-23 16:52:53 -0300

    Update download_instagram.py

--- download_instagram.py
+++ download_instagram.py
@@ -1,42 +1,49 @@
 import instaloader
-import re
+from flask import Flask, request, jsonify
 import os
 import sys
+import re
 
-# Verificar se a URL foi informada corretamente
-try:
-    url = sys.argv[1]
-except IndexError:
-    print("Forma de uso:\n\n", sys.argv[0], "URL\n\nInforme uma URL válida\n\n")
-    sys.exit()
+app = Flask(__name__)
 
-# Diretório de download
-downloadDir = '/home/fabio/Downloads'
-os.chdir(downloadDir)
+@app.route('/download', methods=['POST'])
+def download_video():
+    # Verifica se o URL do vídeo foi enviado
+    video_url = request.form.get('videoUrl')
+    if not video_url:
+        return jsonify({'error': 'URL do vídeo não foi fornecido'}), 400
 
-# Inicializar Instaloader
-loader = instaloader.Instaloader(
-    download_pictures=True, 
-    download_videos=True, 
-    download_video_thumbnails=False, 
-    download_geotags=False,
-    download_comments=False, 
-    save_metadata=False, 
-    compress_json=False,
-    filename_pattern='{profile}_{mediaid}'
-)
+    # Diretório de download
+    download_dir = '/caminho/para/seu/diretorio/de/download'
+    os.chdir(download_dir)
 
-# Extrair "shortcode" da URL
-expr = r'\/p\/([^\/]*)\/'
-found = re.search(expr, url)
+    # Inicializa o Instaloader
+    loader = instaloader.Instaloader(
+        download_pictures=False,
+        download_videos=True,
+        download_video_thumbnails=False,
+        download_geotags=False,
+        download_comments=False,
+        save_metadata=False,
+        compress_json=False,
+        filename_pattern='{profile}_{mediaid}'
+    )
 
-if found:
-    print("Baixando post com shortcode:", found.group(1))
-    try:
-        post = instaloader.Post.from_shortcode(loader.context, found.group(1))
-        loader.download_post(post, target='.')
-        print("Download concluído.")
-    except instaloader.exceptions.InstaloaderException as e:
-        print(f"Erro ao baixar o post: {str(e)}")
-else:
-    print("URL do Instagram inválida ou shortcode não encontrado.")
+    # Extrai o shortcode do URL do vídeo
+    expr = r'\/p\/([^\/]*)\/'
+    found = re.search(expr, video_url)
+
+    if found:
+        try:
+            # Baixa o post com base no shortcode
+            post = instaloader.Post.from_shortcode(loader.context, found.group(1))
+            loader.download_post(post, target='.')
+            return jsonify({'message': 'Download do vídeo concluído'}), 200
+        except instaloader.exceptions.InstaloaderException as e:
+            return jsonify({'error': f'Erro ao baixar o vídeo: {str(e)}'}), 500
+    else:
+        return jsonify({'error': 'URL do Instagram inválido ou shortcode não encontrado'}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
+                                                                               
